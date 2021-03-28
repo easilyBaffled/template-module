256920604027b8f0de60dba080759bdb5fc51b4f
Danny Michaelis <dmichaelis0@gmail.com>
2021-03-28 16:27:37 -0400

    fix formatting config files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 /**
  *
- * @param {string} red
- * @param {string} green
- * @param {string} blue
+ * @param {string|number} red
+ * @param {string|number} green
+ * @param {string|number} blue
  *
  * @returns {string}
  */
-export const rgbToHex = (red, green, blue) => {
-  const redHex = red.toString(16);
-  const greenHex = green.toString(16);
-  const blueHex = blue.toString(16);
+export const rgbToHex = ( red, green, blue ) => {
+    const redHex = red.toString( 16 );
+    const greenHex = green.toString( 16 );
+    const blueHex = blue.toString( 16 );
 
-  return pad(redHex) + pad(greenHex) + pad(blueHex);
+    return pad( redHex ) + pad( greenHex ) + pad( blueHex );
+};
+
+/**
+ *
+ * @param {string} hex
+ * @returns {string[]}
+ */
+export const hexToRgb = ( hex ) => {
+    const red = parseInt( hex.substring( 0, 2 ), 16 );
+    const green = parseInt( hex.substring( 2, 4 ), 16 );
+    const blue = parseInt( hex.substring( 4, 6 ), 16 );
+
+    return [ red, green, blue ];
 };
 
 /**
@@ -19,17 +32,4 @@ export const rgbToHex = (red, green, blue) => {
  * @param {string} hex
  * @returns {string}
  */
-export const hexToRgb = (hex) => {
-  const red = parseInt(hex.substring(0, 2), 16);
-  const green = parseInt(hex.substring(2, 4), 16);
-  const blue = parseInt(hex.substring(4, 6), 16);
-
-  return [red, green, blue];
-};
-
-/**
- *
- * @param {string} hex
- * @returns {string}
- */
-const pad = (hex) => (hex.length === 1 ? "0" + hex : hex);
+const pad = ( hex ) => ( hex.length === 1 ? "0" + hex : hex );
